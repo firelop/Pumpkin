31d466a71bd8218ab54c7249f8c1e633ed44e3e4
Pierre GUCHET <firelop2contact@gmail.com>
2025-06-10 00:38:07 +0200

    Reset fall distance for entities in water or powder snow

--- pumpkin/src/entity/living.rs
+++ pumpkin/src/entity/living.rs
@@ -238,9 +238,17 @@ impl LivingEntity {
                     .await;
             }
         } else if height_difference < 0.0 {
-            let distance = self.fall_distance.load();
+            let mut new_fall_distance = 0f32;
+            
+            // Reset fall distance if is in water or powder_snow
+            if !self.is_in_water().await && !self.is_in_powder_snow().await {
+                let distance = self.fall_distance.load();
+                new_fall_distance = distance - (height_difference as f32)
+            
+            }
+
             self.fall_distance
-                .store(distance - (height_difference as f32));
+                .store(new_fall_distance);
         }
     }
 
